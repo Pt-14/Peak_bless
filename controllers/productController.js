99943508a20c668fd9e431dd16d0d43a0c35403f
Trương Phương <phuongtruong177853@gmail.com>
2025-12-11 09:05:51 +0700

    update logic product

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,11 +1,23 @@
-const fs = require("bbbbbbbbbb");
+const fs = require("fs");
 
 const load = () => JSON.parse(fs.readFileSync("products.json"));
 const save = (data) => fs.writeFileSync("products.json", JSON.stringify(data, null, 2));
 
 module.exports = {
   getAll(req, res) {
-    res.json(load());
+    try {
+      const products = load();
+      // Chỉ lấy các field cần thiết: Id, Name, Price, Image
+      const productList = products.map(p => ({
+        id: p.id,
+        name: p.name,
+        price: p.price,
+        image: p.image
+      }));
+      res.json(productList);
+    } catch (error) {
+      res.status(500).json({ error: "Không thể tải danh sách sản phẩm" });
+    }
   },
 
   add(req, res) {
@@ -32,10 +44,5 @@ module.exports = {
     products[index] = { ...products[index], ...req.body };
     save(products);
     res.json(products[index]);
-    console.log("Load products");
-    console.log("Add product");
-    console.log("Delete product");
-    console.log("Update product");
-    console.log("Product operations completed");
   }
 };
